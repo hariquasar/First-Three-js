4670289659ef494c082b52b4846eeb12ade80cd0
hariquasar <harrythedoctorofficial@gmail.com>
2024-01-29 22:32:23 +0800

    lesson 5

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
 import gsap from 'gsap'
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
 
 const gui = new dat.GUI()
 const world = {
   plane: {
-    width: 16,
-    height: 16,
-    widthSegments: 25,
-    heightSegments: 25
+    width: 400,
+    height: 400,
+    widthSegments: 50,
+    heightSegments: 50
   }
 }
-gui.add(world.plane, 'width', 1, 30).onChange(generatePlane)
-gui.add(world.plane, 'height', 1, 30).onChange(generatePlane)
-gui.add(world.plane, 'widthSegments', 1, 50).onChange(generatePlane)
-gui.add(world.plane, 'heightSegments', 1, 50).onChange(generatePlane)
+gui.add(world.plane, 'width', 1, 500).onChange(generatePlane)
+
+gui.add(world.plane, 'height', 1, 500).onChange(generatePlane)
+gui.add(world.plane, 'widthSegments', 1, 100).onChange(generatePlane)
+gui.add(world.plane, 'heightSegments', 1, 100).onChange(generatePlane)
 
 function generatePlane() {
   planeMesh.geometry.dispose()
@@ -26,6 +27,7 @@ function generatePlane() {
     world.plane.heightSegments
   )
 
+  // vertice position randomization
   const { array } = planeMesh.geometry.attributes.position
   const randomValues = []
   for (let i = 0; i < array.length; i++) {
@@ -33,48 +35,46 @@ function generatePlane() {
       const x = array[i]
       const y = array[i + 1]
       const z = array[i + 2]
-      array[i] = x + Math.random() - 0.5
-      array[i + 1] = y + Math.random() - 0.5
-      array[i + 2] = z + Math.random()
-    }
 
+      array[i] = x + (Math.random() - 0.5) * 3
+      array[i + 1] = y + (Math.random() - 0.5) * 3
+      array[i + 2] = z + (Math.random() - 0.5) * 3
+    }
 
     randomValues.push(Math.random() * Math.PI * 2)
   }
 
   planeMesh.geometry.attributes.position.randomValues = randomValues
-  planeMesh.geometry.attributes.position.originalPosition = planeMesh.geometry.attributes.position.array
+  planeMesh.geometry.attributes.position.originalPosition =
+    planeMesh.geometry.attributes.position.array
 
-
-  const color = []
+  const colors = []
   for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
-    color.push(0, 0.19, 0.4)
+    colors.push(0, 0.19, 0.4)
   }
 
-  planeMesh.geometry.setAttribute('color',
-    new THREE.BufferAttribute(new Float32Array(color), 3))
+  planeMesh.geometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  )
 }
 
-const rayCaster = new THREE.Raycaster()
+const raycaster = new THREE.Raycaster()
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
+  innerWidth / innerHeight,
   0.1,
   1000
 )
 const renderer = new THREE.WebGLRenderer()
-const mouse = {
-  x: undefined,
-  y: undefined
-}
 
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
 new OrbitControls(camera, renderer.domElement)
-camera.position.z = 5
+camera.position.z = 50
 
 const planeGeometry = new THREE.PlaneGeometry(
   world.plane.width,
@@ -84,14 +84,12 @@ const planeGeometry = new THREE.PlaneGeometry(
 )
 const planeMaterial = new THREE.MeshPhongMaterial({
   side: THREE.DoubleSide,
-  flatShading: true,
+  flatShading: THREE.FlatShading,
   vertexColors: true
 })
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
 scene.add(planeMesh)
-
 generatePlane()
-
 
 const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(0, 1, 1)
@@ -101,10 +99,11 @@ const backLight = new THREE.DirectionalLight(0xffffff, 1)
 backLight.position.set(0, -1, -1)
 scene.add(backLight)
 
-const startGeometry = new THREE.BufferGeometry()
+const starGeometry = new THREE.BufferGeometry()
 const starMaterial = new THREE.PointsMaterial({
   color: 0xffffff
 })
+
 const starVerticies = []
 for (let i = 0; i < 10000; i++) {
   const x = (Math.random() - 0.5) * 2000
@@ -113,31 +112,47 @@ for (let i = 0; i < 10000; i++) {
   starVerticies.push(x, y, z)
 }
 
-startGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVerticies, 3))
+starGeometry.setAttribute(
+  'position',
+  new THREE.Float32BufferAttribute(starVerticies, 3)
+)
 
-const stars = new THREE.Points(startGeometry, starMaterial)
+
+const stars = new THREE.Points(starGeometry, starMaterial)
 scene.add(stars)
 
-let frame = 0
+const mouse = {
+  x: undefined,
+  y: undefined
+}
 
+let frame = 0
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
+  raycaster.setFromCamera(mouse, camera)
   frame += 0.01
 
-  const { array, originalPosition, randomValues } = planeMesh.geometry.attributes.position
+  const {
+    array,
+    originalPosition,
+    randomValues
+  } = planeMesh.geometry.attributes.position
   for (let i = 0; i < array.length; i += 3) {
-    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.001
-    array[i + 1] = originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * 0.001
+    // x
+    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01
+
+    // y
+    array[i + 1] =
+      originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * 0.001
   }
 
   planeMesh.geometry.attributes.position.needsUpdate = true
 
-  rayCaster.setFromCamera(mouse, camera)
-  const intersects = rayCaster.intersectObject(planeMesh)
+  const intersects = raycaster.intersectObject(planeMesh)
   if (intersects.length > 0) {
-
     const { color } = intersects[0].object.geometry.attributes
+
     color.setX(intersects[0].face.a, 0.1)
     color.setY(intersects[0].face.a, 0.5)
     color.setZ(intersects[0].face.a, 1)
@@ -154,12 +169,13 @@ function animate() {
 
     const initialColor = {
       r: 0,
-      g: .19,
+      g: 0.19,
       b: 0.4
     }
+
     const hoverColor = {
       r: 0.1,
-      g: .5,
+      g: 0.5,
       b: 1
     }
 
@@ -167,18 +183,23 @@ function animate() {
       r: initialColor.r,
       g: initialColor.g,
       b: initialColor.b,
+      duration: 1,
       onUpdate: () => {
+        // vertice 1
         color.setX(intersects[0].face.a, hoverColor.r)
         color.setY(intersects[0].face.a, hoverColor.g)
         color.setZ(intersects[0].face.a, hoverColor.b)
 
+        // vertice 2
         color.setX(intersects[0].face.b, hoverColor.r)
         color.setY(intersects[0].face.b, hoverColor.g)
         color.setZ(intersects[0].face.b, hoverColor.b)
 
+        // vertice 3
         color.setX(intersects[0].face.c, hoverColor.r)
         color.setY(intersects[0].face.c, hoverColor.g)
         color.setZ(intersects[0].face.c, hoverColor.b)
+        color.needsUpdate = true
       }
     })
   }
@@ -188,22 +209,19 @@ function animate() {
 
 animate()
 
-
-
-addEventListener('mousemove', e => {
-  mouse.x = (e.clientX / innerWidth) * 2 - 1
-  mouse.y = -(e.clientY / innerHeight) * 2 + 1
-
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1
 })
 
-gsap.to('#Stemwork',{
+gsap.to('#Stemwork', {
   opacity: 1,
   duration: 1.5,
   y: 0,
   ease: 'expo'
 })
 
-gsap.to('#text1',{
+gsap.to('#text1', {
   opacity: 1,
   duration: 1.5,
   delay: 0.3,
@@ -211,18 +229,48 @@ gsap.to('#text1',{
   ease: 'expo'
 })
 
-gsap.to('#ahref1',{
+gsap.to('#ahref1', {
   opacity: 1,
   duration: 1.5,
-  y: 0,
   delay: 0.6,
+  y: 0,
   ease: 'expo'
 })
 
-gsap.to('#button1',{
+gsap.to('#button1', {
   opacity: 1,
   duration: 1.5,
-  y: 0,
   delay: 0.9,
+  y: 0,
   ease: 'expo'
+})
+
+document.querySelector('#button1').addEventListener('click', (e) => {
+  e.preventDefault()
+  gsap.to('#container', {
+    opacity: 0
+  })
+  gsap.to(camera.position, {
+    z: 25,
+    ease: 'power3.inOut',
+    duration: 2
+  })
+  gsap.to(camera.rotation, {
+    x: Math.PI/2,
+    ease: 'power3.inOut',
+    duration: 2
+  })
+  gsap.to(camera.position, {
+    y: 1500,
+    ease: 'power3.in',
+    duration: 1,
+    delay: 2
+  })
+})
+
+addEventListener('resize',()=>{
+  // camera.aspect(innerWidth/ innerHeight)
+  camera.aspect = innerWidth/ innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(innerWidth, innerHeight)
 })
